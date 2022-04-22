45576e51ed83a690eb0167780a56860c9438dcaa
Thomas <99062905@mydavinci.nl>
2022-04-22 09:29:22 +0200

    jukebox

---
+++ example-app/resources/views/about.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+    <div>
+        <nav><a href="/about">about</a></nav>
+        <nav><a href="/contact">contact</a></nav>
+    </div>
+        <div>
+            <h1>About page</h1>
+        </div>
+        <div>
+            <p>This is the about page</p>
+        </div>
+    </body>
+</html>
